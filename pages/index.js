08f90d487e938fa6b0a1a79dff99d0a86ea90868
hdriqi <riqi.asdf@gmail.com>
2020-08-04 09:13:46 +0700

    add testimonials

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,31 @@ const slides = [
   }
 ]
 
+const methods = [
+  {
+    name: 'Metode 1',
+    img: `https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg`,
+    desc: ``
+  },
+  {
+    name: 'Metode 2',
+    img: `https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg`
+  }
+]
+
+const testimonials = [
+  {
+    name: 'John Doe',
+    img: `https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg`,
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in mattis justo.`
+  },
+  {
+    name: 'Bambang Suparno',
+    img: `https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg`,
+    message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in mattis justo.`
+  }
+]
+
 const whyElectrum = [
   {
     icon: 'https://evius-dev.s3-ap-southeast-1.amazonaws.com/why-us-electrum-icon.svg',
@@ -105,7 +130,7 @@ export default function Home() {
           {
             whyElectrum.map(data => {
               return (
-                <div className="w-1/2 lg:w-1/4 px-3 mt-12">
+                <div className="w-1/2 lg:w-1/4 px-3 mt-12 lg:mt-20">
                   <div>
                     <div className="relative">
                       <div className="absolute w-full" style={{
@@ -132,17 +157,91 @@ export default function Home() {
           }
         </div>
       </div>
-      <div className="bg-primary-navy">
-        <div className="max-w-6xl m-auto px-4 py-16">
+      <div className="bg-primary-navy overflow-hidden relative">
+        <div className="hidden lg:block absolute h-full w-1/2 z-10 bg-primary-navy"></div>
+        <div className="max-w-6xl m-auto py-16">
           <div className="flex flex-wrap">
-            <div className="w-full lg:w-1/2">
+            <div className="w-full lg:w-1/2 relative z-10 px-4 pt-8">
               <h4 className="text-2xl text-white font-bold">Menggunakan Metode Pembelajaran</h4>
               <h3 className="text-3xl text-white font-bold pt-4">Nama Metode</h3>
             </div>
             <div className="w-full lg:w-1/2 pt-8">
-              <img src="https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg" />
+              <CarouselProvider
+                naturalSlideWidth={100}
+                naturalSlideHeight={75}
+                totalSlides={slides.length}
+                className="carousel-peek"
+              >
+                <Slider>
+                  {
+                    methods.map(method => {
+                      return (
+                        <Slide>
+                          <div className="px-4">
+                            <img className="w-full h-full object-cover rounded-md overflow-hidden" src={method.img} />
+                          </div>
+                        </Slide>
+                      )
+                    })
+                  }
+                </Slider>
+              </CarouselProvider>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="bg-gray-300 py-16">
+        <div className="max-w-4xl m-auto px-4 py-8 lg:p-8 bg-white rounded-md overflow-hidden">
+          <h3 className="text-3xl font-bold text-center">Apa Kata Teman-teman</h3>
+          <CarouselProvider
+            isIntrinsicHeight={true}
+            totalSlides={slides.length}
+            className="mt-8 relative"
+          >
+            <Slider>
+              {
+                testimonials.map(testimony => {
+                  return (
+                    <Slide>
+                      <div className="flex justify-center px-4">
+                        <div className="text-center">
+                          <img className="w-20 h-20 m-auto object-cover rounded-full overflow-hidden" src={testimony.img} />
+                          <h4 className="mt-4 font-semibold text-lg">{testimony.name}</h4>
+                          <p className="mt-2 max-w-md m-auto">{testimony.message}</p>
+                        </div>
+                      </div>
+                    </Slide>
+                  )
+                })
+              }
+            </Slider>
+            <div className="absolute h-full left-0 top-0">
+              <div className="flex items-center h-full px-2 lg:px-8">
+                <ButtonBack>
+                  <svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.6">
+                      <circle cx="24" cy="24" r="24" fill="#575757" fill-opacity="0.2" />
+                      <path d="M21 24L26 29L26.7 28.3L22.4 24L26.7 19.7L26 19L21 24Z" fill="#575757" />
+                    </g>
+                  </svg>
+
+                </ButtonBack>
+              </div>
+            </div>
+            <div className="absolute h-full right-0 top-0">
+              <div className="flex items-center h-full px-2 lg:px-8">
+                <ButtonNext>
+                  <svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g opacity="0.6">
+                      <circle cx="24" cy="24" r="24" fill="#575757" fill-opacity="0.2" />
+                      <path d="M27 24L22 29L21.3 28.3L25.6 24L21.3 19.7L22 19L27 24Z" fill="#575757" />
+                    </g>
+                  </svg>
+
+                </ButtonNext>
+              </div>
+            </div>
+          </CarouselProvider>
         </div>
       </div>
       <div className="bg-primary-navy">

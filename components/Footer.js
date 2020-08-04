f1cd332c019456b4b14a767d6564a4a3bcd031db
hdriqi <riqi.asdf@gmail.com>
2020-08-04 07:58:01 +0700

    add blog post

--- components/Footer.js
+++ components/Footer.js
@@ -9,42 +9,44 @@ const footer = {
 
 const Footer = () => {
   return (
-    <div className="max-w-6xl px-4 m-auto py-16">
-      <div className="flex flex-wrap -mx-3">
-        <div className="w-full lg:w-1/12 px-3 pt-8 lg:pt-0">
-          Logo
+    <div className=" border-t border-gray-300 bg-white">
+      <div className="max-w-6xl px-4 m-auto py-16">
+        <div className="flex flex-wrap -mx-3">
+          <div className="w-full lg:w-1/12 px-3 pt-8 lg:pt-0">
+            Logo
         </div>
-        <div className="w-full lg:w-4/12 px-3 pt-8 lg:pt-0">
-          <h4 className="text-lg font-semibold">Kantor Kami</h4>
-          <div className="mt-2">
-            <p>{footer.address}</p>
+          <div className="w-full lg:w-4/12 px-3 pt-8 lg:pt-0">
+            <h4 className="text-lg font-semibold">Kantor Kami</h4>
+            <div className="mt-2">
+              <p>{footer.address}</p>
+            </div>
           </div>
-        </div>
-        <div className="w-full lg:w-4/12 px-3 pt-8 lg:pt-0">
-          <h4 className="text-lg font-semibold">Hubungi Kami</h4>
-          <div className="mt-2">
-            <p>Email: {footer.email}</p>
-            <p className="mt-1">Phone: {footer.phoneNumber}</p>
+          <div className="w-full lg:w-4/12 px-3 pt-8 lg:pt-0">
+            <h4 className="text-lg font-semibold">Hubungi Kami</h4>
+            <div className="mt-2">
+              <p>Email: {footer.email}</p>
+              <p className="mt-1">Phone: {footer.phoneNumber}</p>
+            </div>
           </div>
-        </div>
-        <div className="w-full lg:w-3/12 px-3 pt-8 lg:pt-0">
-          <h4 className="text-lg font-semibold">Informasi</h4>
-          <div className="mt-2 flex flex-col">
-            <Link href="/blog">
-              <a>Berita dan Event</a>
-            </Link>
-            <Link href="/about-us">
-              <a className="mt-1">Tentang Kami</a>
-            </Link>
-            <Link href="/about-us">
-              <a className="mt-1">Pendaftaran Siswa</a>
-            </Link>
-            <Link href="/about-us">
-              <a className="mt-1">Pendaftaran Tutor</a>
-            </Link>
-            <Link href="/about-us">
-              <a className="mt-1">FAQ</a>
-            </Link>
+          <div className="w-full lg:w-3/12 px-3 pt-8 lg:pt-0">
+            <h4 className="text-lg font-semibold">Informasi</h4>
+            <div className="mt-2 flex flex-col">
+              <Link href="/blog">
+                <a>Berita dan Event</a>
+              </Link>
+              <Link href="/about-us">
+                <a className="mt-1">Tentang Kami</a>
+              </Link>
+              <Link href="/about-us">
+                <a className="mt-1">Pendaftaran Siswa</a>
+              </Link>
+              <Link href="/about-us">
+                <a className="mt-1">Pendaftaran Tutor</a>
+              </Link>
+              <Link href="/about-us">
+                <a className="mt-1">FAQ</a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
---
+++ pages/blog/[id].js
@@ -0,0 +1,37 @@
+import Nav from '../../components/Nav'
+import Footer from '../../components/Footer'
+
+const data = {
+  title: 'Bagaimana cara melakukan pendaftaran?',
+  img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+  createdAt: new Date().getTime()
+}
+
+const Post = () => {
+  return (
+    <div className="bg-gray-100">
+      <Nav />
+      <div className="relative h-screen" style={{
+        minHeight: `16rem`,
+        maxHeight: `20rem`
+      }}>
+        <div className="absolute top-0 w-full h-full z-10" style={{
+          backgroundImage: `url(${data.img})`,
+          backgroundSize: `cover`,
+          backgroundPosition: `center`
+        }}></div>
+      </div>
+      <div className="px-2 pb-16">
+        <div className="max-w-3xl m-auto bg-white rounded-md p-4 lg:p-8 -mt-16 relative z-10">
+          <h2 className="text-xl lg:text-4xl font-semibold">{data.title}</h2>
+          <p className="mt-2">{new Date(Number(data.createdAt)).toLocaleString()}</p>
+          <p className="mt-4">{data.body}</p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Post
